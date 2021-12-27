6e25bf74b525d49d20ebfd16e1b95d2df55b8eed
Proxypepe <sasha.golovin.51@bk.ru>
2021-12-28 00:56:59 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello"}
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
